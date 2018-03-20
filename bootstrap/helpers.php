aa220f49d043a391d3b5d6ed65f9ce76f547f62b
insunshine <292228108@qq.com>
2018-03-20 09:06:12 +0000

    Admin can manage content

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -51,3 +51,38 @@ function write_text_to_log($text, $path = '/Log/', $ext = 'txt')
     $msg = date('H:i:s') . '  ' . $text . PHP_EOL;
     file_put_contents($file_path, $msg, FILE_APPEND);
 }
+
+function model_admin_link($title, $model)
+{
+    return model_link($title, $model, 'admin');
+}
+
+function model_link($title, $model, $prefix = '')
+{
+    //获取数据模型的复数蛇形命名
+    $model_name = model_plural_name($model);
+
+    //初始化前缀
+    $prefix = $prefix ? "/$prefix/" : "/";
+
+    //使用站点URL拼接全量URL
+    $url = config('app.url') . $prefix . $model_name . '/' . $model->id;
+
+    //拼接HTML A 标签，并返回
+    return '<a href="' . $url . '"target=_blank">' . $title . '</a>';
+}
+
+function model_plural_name($model)
+{
+    //从实体中获取完整类名，比兔 App\Models\User
+    $full_class_name = get_class($model);
+
+    //获取基础类名，例如传参`App\Models\User`会得到`User`
+    $class_name = class_basename($full_class_name);
+
+    //蛇形命名，例如：传参`User`会得到`user`,`FooBar`得到`foo_bar`
+    $snake_case_name = snake_case($class_name);
+
+    //获取子串的复数形式
+    return str_plural($snake_case_name);
+}
--- config/administrator/topics.php
+++ config/administrator/topics.php
@@ -1,6 +1,100 @@
 <?php
-/**
- * 用于：
- * author： Xiaoxiaowei
- * Date：  2018-03-13 17:12
- */
+use App\Models\Topic;
+
+return [
+    'title' => '话题',
+    'single' => '话题',
+    'model' => Topic::class,
+
+    'columns' => [
+        'id' => [
+            'title' => 'ID',
+        ],
+        'title' => [
+            'title' => '话题',
+            'sortable' => false,
+            'output' => function($value, $model) {
+                return '<div style="max-width:260px">' . model_link($value, $model) . '</div>';
+            },
+        ],
+        'user' => [
+            'title' => '作者',
+            'sortable' => false,
+            'output' => function($value, $model) {
+                $avatar = $model->user->avatar;
+                $value = empty($avatar) ? 'N/A' : '<img src="' . $avatar . '" style="width:22px;height:22px;"> ' . $model->user->name;
+                return model_link($value, $model);
+            },
+        ],
+        'category' => [
+            'title' => '分类',
+            'sortable' => false,
+            'output' => function($value, $model) {
+                return model_admin_link($model->category->name, $model->category);
+            },
+        ],
+        'reply_count' => [
+            'title' => '评论',
+        ],
+        'operation' => [
+            'title' => '管理',
+            'sortable' => false,
+        ],
+    ],
+    'edit_fields' => [
+        'title' => '标题',
+        'user' => [
+            'title' => '用户',
+            'type' => 'relationship',
+            'name_field' => 'name',
+
+            //自动补全
+            'autocomplete' => true,
+
+            //自动补全搜索字段
+            'search_fields' => ["CONCAT(id, ' ', name)"],
+
+            //自动补全排序
+            'options_sort_field' => 'id',
+        ],
+        'category' => [
+            'title' => '分类',
+            'type' => 'relationship',
+            'name_field' => 'name',
+            'search_fields' => ["CONCAT(id, ' ', name)"],
+            'options_sort_field' => 'id',
+        ],
+        'reply_count' => [
+            'title' => '评论',
+        ],
+        'view_count' => [
+            'title' => '查看',
+        ],
+    ],
+    'filters' => [
+        'id' => [
+            'title' => 'ID',
+        ],
+        'user' => [
+            'title' => '用户',
+            'type' => 'relationship',
+            'name_field' => 'name',
+            'autocomplete' => true,
+            'search_fields' => array("CONCAT(id, ' ', name)"),
+            'options_sort_field' => 'id',
+        ],
+        'category' => [
+            'title' => '分类',
+            'type' => 'relationship',
+            'name_field' => 'name',
+            'search_fields' => array("CONCAT(id, ' ', name)"),
+            'options_sort_field' => 'id',
+        ],
+    ],
+    'rules' => [
+        'title' => 'required',
+    ],
+    'message' => [
+        'title.required' => '请填写标题',
+    ],
+];
