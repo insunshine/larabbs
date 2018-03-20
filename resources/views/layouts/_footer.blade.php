00884250ca6cb829d03464bd2acb9057d5123513
insunshine <292228108@qq.com>
2018-03-20 09:37:45 +0000

    User can manage setting

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,7 +5,7 @@
         </p>
 
         <p class="pull-right">
-            <a href="mailto:292228108@qq.com">link me</a>
+            <a href="mailto:{{ setting('contact_email', '1047723442@qq.com') }}">link me</a>
         </p>
     </div>
 </footer>
